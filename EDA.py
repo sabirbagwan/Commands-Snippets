ae2d1a9e32f3f4ddc3ffdd3d5f6194b7e1e587e4
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2023-04-24 13:38:35 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-columns = ['col1', 'col2']
+columns = ['col1', 'cole2']
 
 fig, axs = plt.subplots(len(columns), 1, figsize=(15, 6*len(columns)))
 
@@ -11,6 +11,13 @@ for i, col in enumerate(columns):
     ax = sns.countplot(x=data[col], order=data[col].value_counts().sort_values(ascending=False).index, ax=axs[i])
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha = 'right')
     ax.set_title(col)
-
+    ax.set_title(col, fontsize=18, fontweight='bold', fontfamily='Arial')
+    ax.set_xlabel(col, fontsize=16, fontfamily='Arial')
+    ax.set_ylabel('Count', fontsize=16, fontfamily='Arial')
+    
+    labels = data[col].value_counts().sort_values(ascending = False).values
+    ax.bar_label(ax.containers[0], labels=labels, label_type='edge')
+    
+    
 plt.tight_layout()
 plt.show()
